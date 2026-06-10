498c9ed4d41e6f1beaa6fac298f37d71571e3471
Köle <kole@tulpara.com>
2026-06-10 10:46:39 +0000

    Set Content-Type for apple-app-site-association

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,19 @@
 import type { NextConfig } from "next";
-const nextConfig: NextConfig = {};
+
+const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/.well-known/apple-app-site-association",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/json",
+          },
+        ],
+      },
+    ];
+  },
+};
+
 export default nextConfig;
